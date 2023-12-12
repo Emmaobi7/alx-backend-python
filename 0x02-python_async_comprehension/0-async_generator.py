c19cbfe8c68c947ff3be22f8a8ca61edcdf5ca12
Emmaobi7 <emma46574@gmail.com>
2023-12-12 15:05:05 +0000

    debug task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,10 +5,10 @@ async generator for random numbers
 """
 import asyncio
 from random import uniform
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     async_generator: generates random n
     return: generator
